55a216ed09a06c061d04fcb2fce641b884d60044
Graeme Ludwig <graeme.ludwig@itv.com>
2025-02-09 21:35:48 +0000

    Added ability to edit labels

--- livechart/src/main/scala/livechart/shopping/LiveChart.scala
+++ livechart/src/main/scala/livechart/shopping/LiveChart.scala
@@ -7,6 +7,7 @@ import org.scalajs.dom
 import com.raquo.laminar.api.L.{*, given}
 import livechart.shopping.Main.renderDataTable
 import livechart.shopping.Main.renderDataItem
+import livechart.shopping.Main.renderDataList
 
 object Main:
     val model = new Model
@@ -16,6 +17,7 @@ object Main:
         div(
             h1("Live Chart"),
             renderDataTable(),
+            renderDataList(),
         )
     end appElement
 
@@ -23,7 +25,7 @@ object Main:
         table(
             thead(tr(th("Label"), th("Price"), th("Count"), th("Full Price"), th("Action"))),
             tbody(
-                children <-- dataSignal.split(xxx => xxx.id) {
+                children <-- dataSignal.split(_.id) {
                     (id, initial, itemSignal) => renderDataItem(id, itemSignal)
                 }
             ),
@@ -38,15 +40,71 @@ object Main:
         )
     end renderDataTable
 
+    def makeDataItemUpdater[A](id: DataItemID, f: (DataItem, A) => DataItem): Observer[A] =
+        dataVar.updater[A] { (data, newValue) =>
+            data.map { item => if (item.id == id) then f(item, newValue) else item }
+        }
+    end makeDataItemUpdater
+
+    // inputForString represents an input text whose value is linked to Signal[String], which can be updated through some Observer[String].
+    // This method is a component. It takes a data model value as an argument, and returns a Laminar element manipulating those values.
+    // In Laminar, components are nothing but methods manipulating time-varying data and returning Laminar elements.
+    def inputForString(valueSignal: Signal[String], valueUpdater: Observer[String]): Input =
+        input(
+            typ := "text",
+            value <-- valueSignal,
+            onInput.mapToValue --> valueUpdater
+        )
+    end inputForString
+
+
     def renderDataItem(id: DataItemID, itemSignal: Signal[DataItem]): Element =
         tr(
-            td(child.text <-- itemSignal.map(_.label)),
+            // td(
+            //     input(
+            //         typ := "text",
+            //         value <-- itemSignal.map(_.label),
+            //         // onInput.mapToValue --> { (newLabel: String) =>
+            //         //     dataVar.update { data =>
+            //         //         data.map { item =>
+            //         //           if (item.id == id) then item.copy(label = newLabel) else item    
+            //         //         }
+            //         //     }
+            //         // },
+
+            //         // onInput.mapToValue --> dataVar.updater[String] { (data, newLabel) =>
+            //         //     data.map { item =>
+            //         //         if (item.id == id) then item.copy(label = newLabel) else item    
+            //         //     }
+            //         // }
+
+            //         onInput.mapToValue --> makeDataItemUpdater[String](
+            //             id,
+            //             (item, newLabel) => item.copy(label = newLabel)
+            //         )
+            //     )
+            // ),
+            td(
+                inputForString(
+                    itemSignal.map(_.label),
+                    makeDataItemUpdater[String](id, (item, newLabel) => item.copy(label = newLabel))
+                )
+            ),
             td(child.text <-- itemSignal.map(item =>"%.3f".format(item.price))),
             td(child.text <-- itemSignal.map(_.count)),
             td(child.text <-- itemSignal.map(item => "%.3f".format(item.fullPrice))),
             td(button("-", onClick --> (_ => removeDataItem(id)))),
         )
     end renderDataItem
+
+    def renderDataList(): Element =
+        ul(
+            children <-- dataSignal.split(_.id) { (id, initial, itemSignal) =>
+                li(child.text <-- itemSignal.map(item => s"${item.count} ${item.label}"))
+                }
+
+        )
+    end renderDataList
 end Main
 
 @main
