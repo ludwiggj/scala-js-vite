8b6fffc26f023a6a3d0fa7ae2c33527575a8ce1b
Graeme Ludwig <graeme.ludwig@itv.com>
2025-02-12 23:24:36 +0000

    Added validation to form example

--- livechart/src/main/scala/forms/FormState.scala
+++ livechart/src/main/scala/forms/FormState.scala
@@ -1,13 +1,43 @@
 package forms
 
 import com.raquo.laminar.api.L.{*, given}
+import com.raquo.laminar.tags.HtmlTag
 import org.scalajs.dom
+import org.scalajs.dom.HTMLElement
+import com.raquo.laminar.nodes.ReactiveHtmlElement
 
 // https://laminar.dev/examples/form-state
 case class FormState(
   zip: String = "",
-  description: String = ""  
-)
+  description: String = "",
+  showErrors: Boolean = false
+) {
+    def hasErrors: Boolean = zipError.nonEmpty || descriptionError.nonEmpty
+
+    def zipError: Option[String] = {
+        if (zip.forall(Character.isDigit) && zip.length == 5) {
+            None
+        } else {
+            Some("Zip code must consist of 5 digits")
+        }
+    }
+
+   def descriptionError: Option[String] = {
+        if (description.nonEmpty) {
+            None
+        } else {
+            Some("Description must not be empty.")
+        }
+    }
+
+    def displayError(error: FormState => Option[String]): Option[String] = {
+      if (showErrors) {
+        error(this)
+      } else {
+        None
+      }
+    }
+}
 
 val stateVar = Var(FormState())
 
@@ -16,7 +46,22 @@ val zipWriter = stateVar.updater[String]((state, zip) => state.copy(zip = zip))
 val descriptionWriter = stateVar.updater[String]((state, description) => state.copy(description = description))
 
 val submitter = Observer[FormState] { state =>
-  dom.window.alert(s"State: $state")
+    if (state.hasErrors) {
+        stateVar.update(_.copy(showErrors = true))
+        //dom.window.alert(s"State: $state contains errors!")
+    } else {
+        dom.window.alert(s"State: $state is valid")
+    }
+}
+
+def renderInputRow(error: FormState => Option[String])(mods: Modifier[ReactiveHtmlElement[HTMLElement]]*): HtmlElement = {
+  val errorSignal = stateVar.signal.map(_.displayError(error))
+  div(
+    cls("-inputRow"),
+    cls("x-hasError") <-- errorSignal.map(_.nonEmpty),
+    p(mods),
+    child.maybe <-- errorSignal.map(_.map(err => div(cls("error"), err)))
+  )
 }
 
 val app = div(
@@ -24,7 +69,8 @@ val app = div(
     onSubmit
       .preventDefault
       .mapTo(stateVar.now()) --> submitter,
-    p(
+
+    renderInputRow(_.zipError)(
       label("Zip code: "),
       input(
         placeholder("12345"),
@@ -33,8 +79,15 @@ val app = div(
           onInput.mapToValue.filter(_.forall(Character.isDigit)) --> zipWriter
         )
       ),
+      p(
+        button(
+          typ("button"), // "submit" is the default in HTML
+          "Set SF zip code",
+          onClick.mapTo("94110") --> zipWriter
+        )
+      )
     ),
-    p(
+    renderInputRow(_.descriptionError)(
       label("Description: "),
       input(
         controlled(
@@ -42,9 +95,19 @@ val app = div(
           onInput.mapToValue --> descriptionWriter
         )
       ),
+      p(
+        button(
+          typ("button"), // "submit" is the default in HTML
+          "Clear",
+          onClick.mapTo("") --> descriptionWriter
+        )
+      )
     ),
     p(
       button(typ("submit"), "Submit")
+    ),
+    p(
+        text <-- stateVar.signal.map(_.toString())
     )
   )
 )
