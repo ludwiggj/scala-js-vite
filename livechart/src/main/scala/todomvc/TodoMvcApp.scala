ebbd15e2082ca1a62bd96490d6d83794921770f9
Graeme Ludwig <graeme.ludwig@itv.com>
2025-02-19 20:16:26 +0000

    MVC app - create item command recognised

--- livechart/src/main/scala/todomvc/TodoMvcApp.scala
+++ livechart/src/main/scala/todomvc/TodoMvcApp.scala
@@ -2,11 +2,89 @@ package todomvc
 
 import com.raquo.laminar.api.L.{*, given}
 import org.scalajs.dom
+import org.scalajs.dom.KeyboardEvent
+//import scala.scalajs.js
+//import scala.scalajs.js.annotation.JSImport
 
 object TodoMvcApp:
-    lazy val node: HtmlElement = {
-        p("Hello")
+    //@js.native @JSImport("@find/**/TodoMvcApp.css")
+    //private object Stylesheet extends js.Object
+    //def useImport(importedObject: js.Any): Unit = ()
+    //useImport(Stylesheet)
+
+    // --- Models ---
+    sealed trait Command
+    case class Create(itemText: String) extends Command
+
+    // --- State ---
+
+    // Var-s are reactive state variables suitable for both local state and redux-like global stores.
+    // Laminar uses my library Airstream as its reactive layer https://github.com/raquo/Airstream
+
+    // private val itemsVar = Var(List[TodoItem]())
+
+    // private val filterVar = Var[Filter](ShowAll)
+
+    // private var lastId = 1 // just for auto-incrementing IDs
+
+    private val commandObserver = Observer[Command] {
+      case item@Create(itemText) =>
+        dom.window.alert(s"Create item $item")
+      //   lastId += 1
+      //   if (filterVar.now() == ShowCompleted)
+      //     filterVar.set(ShowAll)
+      //   itemsVar.update(_ :+ TodoItem(id = lastId, text = itemText, completed = false))
+      // case UpdateText(itemId, text) =>
+      //   itemsVar.update(_.map(item => if (item.id == itemId) item.copy(text = text) else item))
+      // case UpdateCompleted(itemId, completed) =>
+      //   itemsVar.update(_.map(item => if (item.id == itemId) item.copy(completed = completed) else item))
+      // case Delete(itemId) =>
+      //   itemsVar.update(_.filterNot(_.id == itemId))
+      // case DeleteCompleted =>
+      //   itemsVar.update(_.filterNot(_.completed))
     }
+
+    // --- Views ---
+    lazy val node: HtmlElement = {
+      // val todoItemsSignal = itemsVar
+      //   .signal
+      //   .combineWith(filterVar.signal)
+      //   .mapN(_ filter _.passes)  
+    
+      div(
+        cls("todoapp"),
+        div(
+          cls("header"),
+          h1("todos"),
+          renderNewTodoInput,
+        ) //,
+        //div(
+        //  hideIfNoItems,
+        //  cls("main"),
+        //  ul(
+        //    cls("todo-list"),
+        //    children <-- todoItemsSignal.split(_.id)(renderTodoItem)
+        //  )
+        //),
+        //renderStatusBar
+      )
+    }
+
+    private def renderNewTodoInput =
+      input(
+        cls("new-todo"),
+        placeholder("What needs to be done?"),
+        autoFocus(true),
+        onEnterPress
+          .mapToValue
+          .filter(_.nonEmpty)
+          .map(Create(_))
+          .setValue("") --> commandObserver
+      )
+
+    // --- Generic helpers ---
+    private val onEnterPress: EventProcessor[KeyboardEvent, KeyboardEvent] =
+      onKeyPress.filter(_.keyCode == dom.KeyCode.Enter)
 
 
 @main
